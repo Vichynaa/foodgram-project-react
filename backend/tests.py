a5444e3c1b668219a0c1445713e154a347486bcd
Андрей Аверьянов <andrbowname@yandex.ru>
2023-08-09 01:23:10 +0300

    fix

--- backend/tests.py
+++ backend/tests.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from rest_framework.test import APIClient
 from rest_framework import status
+from rest_framework.test import APIClient
 
 
 class RecipeAPITestCase(TestCase):
