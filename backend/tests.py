d553d0d110923d1f6b6a79548633866a7154087c
Андрей Аверьянов <andrbowname@yandex.ru>
2023-08-30 23:36:35 +0300

    fix action + txt

--- backend/tests.py
+++ backend/tests.py
@@ -15,15 +15,3 @@ class RecipeAPITestCase(TestCase):
     def test_recipes(self):
         response = self.client.get('/api/recipes/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_tags(self):
-        response = self.client.get('/api/tags/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_ingredients(self):
-        response = self.client.get('/api/ingredients/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_users(self):
-        response = self.client.get('/api/users/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
