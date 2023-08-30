ab5cf54f16f92dfb53fb94305271ecad840283db
Андрей Аверьянов <andrbowname@yandex.ru>
2023-08-30 23:39:08 +0300

    fix action + txt

--- backend/tests.py
+++ backend/tests.py
@@ -1,17 +1,1 @@
-from django.contrib.auth import get_user_model
-from django.test import TestCase
-from rest_framework import status
-from rest_framework.test import APIClient
-
-
-class RecipeAPITestCase(TestCase):
-
-    def setUp(self):
-        User = get_user_model()
-        self.user = User.objects.create_user(username='auth_user')
-        self.client = APIClient()
-        self.client.force_authenticate(user=self.user)
-
-    def test_recipes(self):
-        response = self.client.get('/api/recipes/')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+#
