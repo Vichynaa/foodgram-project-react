5fe29884f4451ded3cd4154bd768946d7a7530ed
Андрей Аверьянов <andrbowname@yandex.ru>
2023-08-31 16:43:03 +0300

    fix problem with Django 4.2

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -125,7 +125,7 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images)1
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
